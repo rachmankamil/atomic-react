27f0d6def871667466b075078bc8c7d5b3b591bc
rachmankamil <rachmankamil24@gmail.com>
2023-09-29 15:35:41 +0700

    navigate after login

--- src/component/organisms/FormLogin.jsx
+++ src/component/organisms/FormLogin.jsx
@@ -2,14 +2,16 @@ import React, { useState, useRef } from "react"
 
 import TextBoxMolecule from "../molecules/TextBoxMolecule"
 import ButtonAtom from "../atoms/ButtonAtom/ButtonAtom"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
     const [username, setUsername] = useState('')
     const password = useRef('')
-
+    const nav = useNavigate()
     function login(){
         alert("username:"+username+" <====> password:"+password.current.value)
         localStorage.setItem("isLogin", "true")
+        nav("/admin")
     }
 
     return(
