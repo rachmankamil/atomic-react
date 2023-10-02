051fb990a9a2418a768133681e441466af9c1cd2
rachmankamil <rachmankamil24@gmail.com>
2023-10-02 16:13:49 +0700

    add axios and post to mockapi.io

--- src/component/organisms/FormProductOrganism.jsx
+++ src/component/organisms/FormProductOrganism.jsx
@@ -5,6 +5,8 @@ import RadioBoxMolecule from "../molecules/RadioBoxMolecule";
 import ButtonAtom from "../atoms/ButtonAtom/ButtonAtom";
 import ModalMolecule from "../molecules/ModalMolecule";
 
+import axios from "axios";
+
 function reducer(state, action){
     switch (action.type) {
         case 'INCREMENT':
@@ -36,6 +38,13 @@ const FormProductOrganism = () => {
 
     const [state, dispatch] = useReducer(reducer, {count : 0, name : "Kamil"})
 
+    const [productForm, setProductForm] = useState({
+        name: "",
+        category: "",
+        color: "",
+        freshness:""
+    })
+
     const [greeting, setGreeting] = useState("")
     const [data, setData] = useState([{
         color: "white",
@@ -64,13 +73,40 @@ const FormProductOrganism = () => {
         console.log(data);
     }
 
+    const insertProduct = () => {
+        axios
+            .post("https://63206412e3bdd81d8ef940ed.mockapi.io/api/v1/product", productForm)
+            .then((response)=>{
+                if (response.status == 201){
+                    alert("Sukses menambahkan data!")
+                }
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+    }
+
     return (
         <>
             <h4>{greeting}. Detail Product {state.count}</h4>
-            <TextBoxMolecule name="product_name" label="Product Name" value={state.name}></TextBoxMolecule>
-            <TextBoxMolecule name="product_category" label="Product Category" value="car"></TextBoxMolecule>
-            <TextBoxMolecule name="product_color" label="Product Color" value={data.color}></TextBoxMolecule>
-            <RadioBoxMolecule name="product_freshness" label="Product Freshness" item={["Brand New", "Second Hand", "reFurbished"]}></RadioBoxMolecule>
+            <div>
+                <TextBoxMolecule name="product_name" label="Product Name" onChange={(val)=>{
+                    setProductForm({name:val.target.value, 
+                    category:productForm.category,
+                    color:productForm.color})
+                    }}></TextBoxMolecule>
+                <TextBoxMolecule name="product_category" label="Product Category" onChange={(val)=>{
+                    setProductForm({name:productForm.name, 
+                        category:val.target.value,
+                        color:productForm.color})
+                    }}></TextBoxMolecule>
+                <TextBoxMolecule name="product_color" label="Product Color" onChange={(val)=>{
+                    setProductForm({name:productForm.name, 
+                        category:productForm.category,
+                        color:val.target.value})}}></TextBoxMolecule>
+                <RadioBoxMolecule name="product_freshness" label="Product Freshness" item={["Brand New", "Second Hand", "reFurbished"]}></RadioBoxMolecule>
+                <ButtonAtom text="submit" onClick={insertProduct} />
+            </div>
             <div>
                 <ButtonAtom text="change name" style={buttonStyle} onClick={()=>{ dispatch({type: 'CHANGENAME'}) }}></ButtonAtom>
                 <ButtonAtom text="count +" style={buttonStyle} onClick={()=>{ dispatch({type: 'INCREMENT'}) }}></ButtonAtom>
